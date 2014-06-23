0afe1e309aba6a8f13bc51999d47b711345fa05d
solos <lxl1217@gmail.com>
2014-06-23 22:33:43 +0800

    1.9 and 1.10
    1.9 and 1.10

---
+++ chapter1/9.scm
@@ -0,0 +1,40 @@
+(define (dec a)
+    (- a 1)
+)
+
+(define (inc a)
+    (+ a 1)
+)
+
+(define (plus a b)
+    (if (= a 0)
+        b
+    (inc (plus (dec a) b))))
+
+(plus 4 5)
+
+;(+ 4 5)
+;(inc (+ 3 5))
+;(inc (inc (+ 2 5)))
+;(inc (inc (inc (+ 1 5))))
+;(inc (inc (inc (inc (+ 0 5)))))
+;
+;(inc (inc (inc (inc 5))))
+;(inc (inc (inc 6)))
+;(inc (inc 7))
+;(inc 8)
+;9
+
+(define (plus a b)
+    (if (= a 0)
+        b
+    (plus (dec a) (inc b))))
+
+(plus 4 5)
+
+;(+ 4 5)
+;(+ 3 6)
+;(+ 2 7)
+;(+ 1 8)
+;(+ 0 9)
+;9
